f9d32f44ecdadcc6c0f872e39f1db06438fe9d1d
Matthias <m.geslin974@gmail.com>
2020-01-08 18:09:42 +0100

    MainController Created
    Basic methods 
    -> construct : twig methods , session, and filter html
    etc

---
+++ src/Controller/MainController.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Controller;
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
+/**
+ * Class MainController
+ * Manages the Main Features
+ * @package App\Controller
+ */
+abstract class MainController extends SuperGlobalsController
+{
+    /**
+     * @var Environment|null
+     */
+    protected $twig = null;
+
+    /**
+     * MainController constructor
+     * Creates the Template Engine & adds its Extensions
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->twig = new Environment(new FilesystemLoader('../src/View'), array(
+            'cache' => false,
+            'debug' => true
+        ));
+        $this->twig->addExtension(new DebugExtension());
+        $this->twig->addGlobal('session', $_SESSION);
+        $this->twig->addFilter( new \Twig\TwigFilter('nl2br', 'nl2br', ['is_safe' => ['html']]));
+    }
+
+    /**
+     * Returns the Page URL
+     * @param string $page
+     * @param array $params
+     * @return string
+     */
+    public function url(string $page, array $params = [])
+    {
+        $params['access'] = $page;
+        return 'index.php?' . http_build_query($params);
+    }
+
+    /**
+     * Redirects to another URL
+     * @param string $page
+     * @param array $params
+     */
+    public function redirect(string $page, array $params = [])
+    {
+        header('Location: ' . $this->url($page, $params));
+        exit;
+    }
+
+    /**
+     * Renders the Views
+     * @param string $view
+     * @param array $params
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function render(string $view, array $params = [])
+    {
+        return $this->twig->render($view, $params);
+    }
+}
