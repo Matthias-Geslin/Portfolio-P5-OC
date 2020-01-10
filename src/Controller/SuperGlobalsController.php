03dbec434e2b7659009d66b1e8be26f8d4a44b83
Matthias <m.geslin974@gmail.com>
2020-01-10 14:22:24 +0100

    Super glob udpate
    added methods for $_FILES super global

--- src/Controller/SuperGlobalsController.php
+++ src/Controller/SuperGlobalsController.php
@@ -21,12 +21,22 @@ abstract class SuperGlobalsController
     /**
      * @var mixed|null
      */
-    private $session = null;
+    protected $session = null;
 
     /**
      * @var mixed
      */
-    private $user = null;
+    protected $user = null;
+
+    /**
+     * @var
+     */
+    protected $files;
+
+    /**
+     * @var
+     */
+    protected $file;
 
     /**
      * SuperGlobalsController constructor
@@ -40,6 +50,11 @@ abstract class SuperGlobalsController
         if (isset($this->session['users']))
         {
             $this->user = $this->session['users'];
+        }
+
+        $this->files = filter_var_array($_FILES);
+        if (isset($this->files['file'])) {
+            $this->file = $this->files['file'];
         }
     }
 
@@ -92,5 +107,52 @@ abstract class SuperGlobalsController
             $this->user[$var] = null;
         }
         return $this->user[$var];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFilesArray()
+    {
+        return $this->files;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFileArray()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param string $var
+     * @return mixed
+     */
+    public function getFileVar(string $var)
+    {
+        return $this->file[$var];
+    }
+
+    /**
+     * @param $fileDir
+     * @return mixed
+     */
+    public function uploadFile(string $fileDir)
+    {
+        try {
+            if (!isset($this->file['error']) || is_array($this->file['error'])) {
+                throw new Exception('Invalid parameters...');
+            }
+            if ($this->file['size'] > 1000000) {
+                throw new Exception('Exceeded filesize limit...');
+            }
+            if (!move_uploaded_file($this->file['tmp_name'], "{$fileDir}/{$this->file['name']}")) {
+                throw new Exception('Failed to move uploaded file...');
+            }
+            return $this->file['name'];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
