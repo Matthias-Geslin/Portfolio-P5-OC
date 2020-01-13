927bfa3a88f26f08ed662540f6faf30e12182d6b
Matthias <m.geslin974@gmail.com>
2020-01-13 10:05:46 +0100

    script created

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    // var slider = new Slider();
+    // slider.begin();
+
+    var password = new Password();
+    password.begin();
+});
